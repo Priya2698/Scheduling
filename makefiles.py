67fb45f752c5853a48c9bd4ab95929aba1daff8d
Priya <priyamis@cse.iitk.ac.in>
2020-11-02 15:42:05 +0530

    setup files

---
+++ makefiles.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+import os
+import sys
+import subprocess
+from shutil import copyfile
+
+os.chdir("slurm-19.05.0/src/plugins/select/linear")
+print(os.getcwd())
+copyfile('Makefile', 'Makefile_default')
+copyfile('Makefile', 'Makefile_greedy')
+copyfile('Makefile', 'Makefile_bal')
+copyfile('Makefile', 'Makefile_ada')
+
+default = open('Makefile_default','w+')
+greedy = open('Makefile_greedy','w+')
+bal = open('Makefile_bal','w+')
+ada = open('Makefile_ada','w+')
+makefile= open('Makefile','r')
+
+line = makefile.readline()
+while line:
+    try:
+        data = line.split()
+        if data[0] !='CFLAGS':
+            default.write(line)
+            greedy.write(line)
+            bal.write(line)
+            ada.write(line)
+
+        else:
+            if data[-1] == '-fno-strict-aliasing':
+                default.write(line)
+                greedy.write(line.rstrip()+" -DJOBAWARE\n")
+                bal.write(line.rstrip()+" -DJOBAWARE1\n")
+                ada.write(line.rstrip()+" -DJOBAWARE2\n")
+            else:
+                default.write(line.rstrip().rsplit(' ', 1)[0]+"\n")
+                greedy.write(line.rstrip().rsplit(' ', 1)[0]+" -DJOBAWARE\n")
+                bal.write(line.rstrip().rsplit(' ', 1)[0]+" -DJOBAWARE1\n")
+                ada.write(line.rstrip().rsplit(' ', 1)[0]+" -DJOBAWARE2\n")
+    except:
+        default.write(line)
+        greedy.write(line)
+        bal.write(line)
+        ada.write(line)
+   
+    line = makefile.readline()
+
+default.close()
+greedy.close()
+bal.close()
+ada.close()
+makefile.close()
---
+++ prepare_run.sh
@@ -0,0 +1,52 @@
+#!bin/bash
+algo=$1
+
+echo "Checking branch, status and setting Makefile"
+cd ~/slurm_changes/src/plugins/select/linear
+if [[ $algo = "default" ]];
+then
+    echo "Default"
+    git checkout master
+    git branch
+    cp Makefile_default Makefile
+
+elif [[ $algo = "greedy" ]];
+then
+    echo "greedy"
+    git checkout master
+    git branch
+    cp Makefile_greedy Makefile
+
+elif [[ $algo = "bal" ]];
+then
+    echo "balanced"
+    git checkout algo2
+    git branch
+    cp Makefile_bal Makefile
+elif [[ $algo = "ada" ]];
+then
+    echo "adaptive"
+    git checkout algo2
+    git branch
+    cp Makefile_ada Makefile
+elif [[ $algo = "inter"]];
+then
+    echo "interference"
+    git checkout interference
+    git branch
+    cp Makefile_greedy Makefile
+fi
+make clean
+make
+cd ../../../..
+
+echo " Running make "
+make
+echo " Running make install "
+sudo make install
+echo " Restarting and checking slurmctld status"
+sudo systemctl restart slurmctld
+sudo systemctl status slurmctld
+echo " Restarting and checking slurmd status"
+sudo systemctl restart slurmd
+sudo systemctl status slurmd
